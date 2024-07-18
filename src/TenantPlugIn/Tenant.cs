76c773df7a192a7e2ffbb5466b744cea5b8c6228
angoudjou <ngoudjou@yahoo.fr>
2024-07-18 15:56:44 -0400

    Add: Fname and LName and DOB

--- src/TenantPlugIn/Tenant.cs
+++ src/TenantPlugIn/Tenant.cs
@@ -4,7 +4,10 @@ public class Tenant
 {
     public string ID { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; } = string.Empty;
+    public string LName { get; set; } = string.Empty;
+    public string FName { get; set; } = string.Empty;
     public DateOnly DateCreat { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    public DateTime DOB { get; set; } =default!;
 
     public Tenant()
     {
