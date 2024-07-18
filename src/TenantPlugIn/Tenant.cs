d9acbe41ba329ac1324191cec3dc8fcad5afe7f9
angoudjou <ngoudjou@yahoo.fr>
2024-07-18 10:40:58 -0400

    Add: Tenant class

---
+++ src/TenantPlugIn/Tenant.cs
@@ -0,0 +1,17 @@
+﻿namespace TenantPlugIn;
+
+public class Tenant
+{
+    public string ID { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; } = string.Empty;
+    public DateOnly DateCreat { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+    public Tenant()
+    {
+            
+    }
+    public Tenant(string name)
+    {
+        Name = name;
+    }
+}
